e75c0c10c1fb419c00ddd550cddfff6cb5aefcd3
Michael Sutherland <mrsutherland@gmail.com>
2014-01-04 16:17:54 -0800

    Added support to extract text from website using readability API.  Need to check on rules before releasing code using this (also this uses a token linked to me).

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -27,30 +27,41 @@ Pebble.addEventListener("appmessage",
       if (post == undefined) {
         console.error("Requested data for post index that doesn't exist: "+index);
       }
+      // TODO: store content for webpage
       // look for selftext in post using index
       if (post.selftext) {
         // return selftext
         console.log("Returning selftext for post: "+post.title);
-        var data = {"req_post_data": 1,
-                    "index": e.index,
+        var data = {"post_data": 1,
+                    "index": index,
                     "data": post.selftext.slice(0, 1024)};
         App.data.add_to_queue(data);
+      } else if (post.domain == "imgur.com" || post.domain == "i.imgur.com") {
+        console.log("imgur post...");
+        // TODO: show thumbnail
+        var data = {"post_data": 1,
+                    "index": index,
+                    "data": "TODO: handle images"};
+        App.data.add_to_queue(data);        
       } else {
         // if selftext doesn't exists, request the actual page and load it.
-        console.log("Requesting url: "+post.url+ "\nfor post: "+post.title);
-        var req = new XMLHttpRequest();
-        req.open('GET', post.url, true);
-        req.onload = function(e) {
-          if (req.readyState == 4 && req.status == 200) {
-            if(req.status == 200) {
-              var data = {"post_data": 1,
-                          "index": index,
-                          "data": req.responseText.slice(0, 1024)};
-              App.data.add_to_queue(data);
-            } else { console.error("Failed to open url: "+post.url); }
-          }
-        };
-        req.send(null);
+        console.log("HTML post for: "+post.url);
+        App.post.get_url_content(post.url, function(response_data) {
+          var content = response_data.content;
+          // remove xml elements from content
+          content = content.replace(/<.*?>/g, "");
+          // remove \n and \t from content
+          content = content.replace(/\n/g, "").replace(/\t/g, "");
+          // remove any special characters
+          content = content.replace(/&#x[0-9a-zA-Z]*?;/g, "");
+          // replace &lt;, &gt;, &amp;
+          content = content.replace(/&lt;/g, "<").replace(/&gt;/g, ">").replace(/&amp;/g, "&");
+          console.log(content);
+          var data = {"post_data": 1,
+                      "index": index,
+                      "data": content.slice(0, 1024)};
+          App.data.add_to_queue(data);
+        });
       }
     }
   }
@@ -110,6 +121,25 @@ App.post.get = function() {
           }
         }
       } else { console.error("Error requesting posts from Reddit"); }
+    }
+  };
+  req.send(null);
+};
+App.post.readability_key = "04a61859f5a6f927d930b0b85ccddcf02ae72630";
+App.post.get_url_content = function(url, onload) {
+  // request content from url
+  console.log("Requesting url: "+url);
+  var req = new XMLHttpRequest();
+  var read_url = "https://www.readability.com/api/content/v1/parser?url="+url+"&token="+App.post.readability_key;
+  req.open('GET', read_url, true);
+  req.onload = function(e) {
+    if (req.readyState == 4 && req.status == 200) {
+      if(req.status == 200) {
+        console.log("Readability response for url: "+req.responseText);
+        onload(JSON.parse(req.responseText));
+      } else {
+        console.error("Failed to open url: "+post.url);
+      }
     }
   };
   req.send(null);
